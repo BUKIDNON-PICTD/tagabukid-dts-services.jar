3dabc6befda85bf73370cbc90e55e0750c2aefb9
RUFINO <aguilarufino@gmail.com>
2016-10-18 15:28:45 +0800

    working new with child support

--- sql/subaydocumenttask.sql
+++ sql/subaydocumenttask.sql
@@ -1,37 +1,37 @@
 [closePrevTask]
-UPDATE document_task dt
-INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
+UPDATE subay_document_task dt
+INNER JOIN subay_document_task_org dto ON dto.`taskid` = dt.`objid`
 SET dt.enddate = $P{enddate}
 WHERE dt.refid = $P{refid} AND dt.enddate IS NULL ${filter}
 
 [deleteTask]
-DELETE FROM document_task WHERE lft > $P{mylft} AND rgt < $P{myrgt} AND refid = $P{refid}
+DELETE FROM subay_document_task WHERE lft > $P{mylft} AND rgt < $P{myrgt} AND refid = $P{refid}
 
 [deleteTaskById]
-DELETE FROM document_task WHERE objid = $P{taskid}
+DELETE FROM subay_document_task WHERE objid = $P{taskid}
 
 [updateDeletedTaskrgt]
-UPDATE document_task SET rgt = rgt - $P{mywidth} WHERE rgt > $P{myrgt} AND refid = $P{refid}
+UPDATE subay_document_task SET rgt = rgt - $P{mywidth} WHERE rgt > $P{myrgt} AND refid = $P{refid}
 
 [updateDeletedTasklft]
-UPDATE document_task SET lft = lft - $P{mywidth} WHERE lft > $P{myrgt} AND refid = $P{refid}
+UPDATE subay_document_task SET lft = lft - $P{mywidth} WHERE lft > $P{myrgt} AND refid = $P{refid}
 
 [findTask]
-SELECT * FROM document_task WHERE objid = $P{objid}
+SELECT * FROM subay_document_task WHERE objid = $P{objid}
 
 [findParentNode]
 SELECT * FROM 
 (SELECT node.*, (COUNT(parent.objid) - 1) AS depth
-FROM document_task AS node,
-document_task AS parent
+FROM subay_document_task AS node,
+subay_document_task AS parent
 WHERE node.lft BETWEEN parent.lft AND parent.rgt
 AND node.refid = $P{refid}
 AND parent.refid = $P{refid}
 GROUP BY node.objid
 ORDER BY node.lft)xx
 WHERE depth = (SELECT (COUNT(parent.objid) - 1) AS depth
-FROM document_task AS node,
-document_task AS parent
+FROM subay_document_task AS node,
+subay_document_task AS parent
 WHERE node.lft BETWEEN parent.lft AND parent.rgt
 AND node.refid = $P{refid}
 AND parent.refid = $P{refid}
@@ -40,32 +40,32 @@ GROUP BY node.objid
 ORDER BY node.lft) - 1;
 
 [updateParentNode]
-UPDATE document_task SET enddate = NULL WHERE objid = $P{taskid}
+UPDATE subay_document_task SET enddate = NULL WHERE objid = $P{taskid}
 
 [cancelSend]
-UPDATE document_task dt
+UPDATE subay_document_task dt
 SET dt.enddate = NULL
 WHERE dt.refid = $P{refid} AND objid = $P{taskid}
 
 [getTaskListByRef2]
 SELECT dt.*,dto.*
 FROM ${taskTablename} dt
-INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
+INNER JOIN subay_document_task_org dto ON dto.`taskid` = dt.`objid`
 WHERE dt.refid=$P{refid} 
 ORDER BY dt.startdate
 
 [getTaskListByRef]
 SELECT xx.*, dto.* 
 FROM (SELECT dtp.*
-FROM document_task dt, document_task dtp
+FROM subay_document_task dt, subay_document_task dtp
 WHERE dt.lft BETWEEN dtp.lft AND dtp.rgt
 AND dt.objid = $P{taskid} 
 AND dtp.refid = $P{refid} 
 ORDER BY dtp.lft)xx
-INNER JOIN document_task_org dto ON dto.`taskid` = xx.`objid`;
+INNER JOIN subay_document_task_org dto ON dto.`taskid` = xx.`objid`;
 
 [attachChildTask]
-UPDATE document_task 
+UPDATE subay_document_task 
 SET enddate = $P{enddate},
 state = $P{state},
 message = $P{message},
@@ -73,26 +73,26 @@ parentprocessid = $P{parentprocessid}
 WHERE refid = $P{refid} AND enddate IS NULL AND state = 'processing'
 
 [findPrevTask]
-SELECT * FROM document_task dt
-INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
+SELECT * FROM subay_document_task dt
+INNER JOIN subay_document_task_org dto ON dto.`taskid` = dt.`objid`
 WHERE dt.refid = $P{refid} AND dt.enddate IS NULL ${filter}
 
 [changeRight]
-UPDATE document_task SET rgt = rgt + 2 WHERE rgt > $P{myleft} AND refid = $P{refid}
+UPDATE subay_document_task SET rgt = rgt + 2 WHERE rgt > $P{myleft} AND refid = $P{refid}
 
 [changeLeft]
-UPDATE document_task SET lft = lft + 2 WHERE lft > $P{myleft} AND refid = $P{refid}
+UPDATE subay_document_task SET lft = lft + 2 WHERE lft > $P{myleft} AND refid = $P{refid}
 
 [getChildTask]
 SELECT xx.*,dto.* FROM (
 SELECT node.*, (COUNT(parent.objid) - (sub_tree.depth + 1)) AS depth
-FROM document_task AS node,
-        document_task AS parent,
-        document_task AS sub_parent,
+FROM subay_document_task AS node,
+        subay_document_task AS parent,
+        subay_document_task AS sub_parent,
         (
                 SELECT node.objid, (COUNT(parent.objid) - 1) AS depth
-                FROM document_task AS node,
-                document_task AS parent
+                FROM subay_document_task AS node,
+                subay_document_task AS parent
                 WHERE node.lft BETWEEN parent.lft AND parent.rgt
                 AND node.`objid` = $P{taskid}
               
@@ -109,22 +109,22 @@ WHERE node.lft BETWEEN parent.lft AND parent.rgt
 	AND sub_parent.refid = $P{refid}
 GROUP BY node.objid
 ORDER BY node.lft)xx
-INNER JOIN document_task_org dto ON dto.`taskid` = xx.`objid`
+INNER JOIN subay_document_task_org dto ON dto.`taskid` = xx.`objid`
 WHERE xx.depth = 1;
 
 [getChildTasks]
 SELECT * FROM 
 (SELECT node.*, (COUNT(parent.objid) - 1) AS depth
-FROM document_task AS node,
-document_task AS parent
+FROM subay_document_task AS node,
+subay_document_task AS parent
 WHERE node.lft BETWEEN parent.lft AND parent.rgt
 AND node.refid = $P{refid}
 AND parent.refid = $P{refid}
 GROUP BY node.objid
 ORDER BY node.lft)xx
 WHERE depth = (SELECT (COUNT(parent.objid) - 1) AS depth
-FROM document_task AS node,
-document_task AS parent
+FROM subay_document_task AS node,
+subay_document_task AS parent
 WHERE node.lft BETWEEN parent.lft AND parent.rgt
 AND node.refid = $P{refid}
 AND parent.refid = $P{refid}
